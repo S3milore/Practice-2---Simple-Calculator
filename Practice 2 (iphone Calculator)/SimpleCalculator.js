c0f38255b2aac253e81d3699ea7db7fa581064dd
Your Name <semiloreawolusi@gmail.com>
2022-10-13 09:42:28 -0400

    calculator javascript

---
+++ Practice 2 (iphone Calculator)/SimpleCalculator.js
@@ -0,0 +1,54 @@
+
+      function clr() {
+        document.getElementById("result").innerHTML = "0";
+      }
+
+      function one() {
+        document.getElementById("result").value += 1;
+      }
+
+      function two() {
+        document.getElementById("result").value += 2;
+      }
+
+      function three() {
+        document.getElementById("result").value += 3;
+      }
+
+      function four() {
+        document.getElementById("result").value += 4;
+      }
+
+      function five() {
+        document.getElementById("result").value += 5;
+      }
+
+      function six() {
+        document.getElementById("result").value += 6;
+      }
+
+      function seven() {
+        document.getElementById("result").value += 7;
+      }
+
+      function eight() {
+        document.getElementById("result").value += 8;
+      }
+
+      function nine() {
+        document.getElementById("result").value += 9;
+      }
+      
+      function division() {
+       // document.getElementById("result").value += /;
+      }
+      function nine() {
+        document.getElementById("result").value += 9;
+      }
+      function nine() {
+        document.getElementById("result").value += 9;
+      }
+
+      //function decimal() {
+        //document.getElementId("result").value ="";
+      //}
